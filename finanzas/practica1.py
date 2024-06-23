2a3ecabfada1db83c037623c48e3d748414bb03b
alexanderabasolo <alexabasolo99@gmail.com>
2024-06-23 15:02:42 +0200

    Add practica1.py for financial practice

---
+++ finanzas/practica1.py
@@ -0,0 +1,21 @@
+import math
+
+def calcular_e(precision):
+    # acumuladores e y num_terminos, termino es la x en el ejercicio
+    e = 0
+    termino = 1
+    num_terminos = 0
+    
+    while abs(termino) > precision:
+        e += termino
+        num_terminos += 1
+        termino = 1 / math.factorial(num_terminos)
+    # e es una aproximacion
+    return e, num_terminos
+
+target_precision = 2.22e-16
+e, num_terminos = calcular_e(target_precision)
+
+# no utilizamos las tildes para que no haya ninguna incidencia
+print(f"La proximación de e resulta ser: {e}.")
+print(f"El numero de terminos utilizados corresponde con: {num_terminos}.")
